df951547ea107192bcb71059f0b07ca3126b4dfc
Danial Manavi <dmanavi@live.com>
2020-08-22 17:19:27 +0430

    Add factory methods.
    Changes to be committed:
    	modified:   Domain/OpeningHour.cs
    	modified:   Domain/Restaurant.cs

--- Domain/OpeningHour.cs
+++ Domain/OpeningHour.cs
@@ -1,7 +1,27 @@
+using System;
+
 namespace NearbyRestaurants.Domain
 {
     public class OpeningHour
     {
-        
+        public DayOfWeek Day { get; private set; }
+
+        public TimeSpan From { get; private set; }
+
+        public TimeSpan To { get; private set; }
+
+        /// <summary>
+        /// Create a new instance of opening hours
+        /// </summary>
+        /// <param name="day">Day of the week</param>
+        /// <param name="from">Start working from</param>
+        /// <param name="to">Continue working to</param>
+        /// <returns>Opening hours</returns>
+        public static OpeningHour Create(DayOfWeek day, TimeSpan from, TimeSpan to) => new OpeningHour()
+        {
+            Day = day,
+            From = from,
+            To = to
+        };
     }
 }
--- Domain/Restaurant.cs
+++ Domain/Restaurant.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using MongoDB.Bson;
 using MongoDB.Driver.GeoJsonObjectModel;
 
@@ -6,12 +7,27 @@ namespace NearbyRestaurants.Domain
 {
     public class Restaurant
     {
-        public ObjectId Id { get; set; }
+        public ObjectId Id { get; private set; }
 
-        public string Title { get; set; }
+        public string Name { get; private set; }
 
-        public ICollection<OpeningHour> OpeningHours { get; set; }
+        public ICollection<OpeningHour> OpeningHours { get; private set; }
 
-        public GeoJsonPoint<GeoJson2DGeographicCoordinates> Type { get; set; }
+        public GeoJsonPoint<GeoJson2DGeographicCoordinates> Location { get; private set; }
+
+        /// <summary>
+        /// Create a new restaurant
+        /// </summary>
+        /// <param name="name">Name of the restaurant</param>
+        /// <param name="openingHours">Opening hours</param>
+        /// <param name="location">Point on the map</param>
+        /// <returns>New instance of the restaurant</returns>
+        public static Restaurant create(string name, IEnumerable<OpeningHour> openingHours,
+            GeoJsonPoint<GeoJson2DGeographicCoordinates> location) => new Restaurant()
+        {
+            Name = name,
+            OpeningHours = openingHours.ToList(),
+            Location = location
+        };
     }
 }
